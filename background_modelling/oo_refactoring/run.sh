aff058db59439b28e6ecab45fe0066450ca0d9c8
noepalm <palmeri.1871005@studenti.uniroma1.it>
2025-10-17 17:54:20 +0200

    Misc updates to running scripts

--- background_modelling/oo_refactoring/run.sh
+++ background_modelling/oo_refactoring/run.sh
@@ -1,2 +1,19 @@
 python main_background_analysis.py --full_analysis --bkg_function 0 \
-                                   --fit_jpsi_prompt --floating_resonant &> log
+                                   --fit_jpsi_prompt --floating_resonant \
+                                   --fit_region="region1" &> logs/reweight_cats_region1_log
+
+python main_background_analysis.py --full_analysis --bkg_function 0 \
+                                   --fit_jpsi_prompt --floating_resonant \
+                                   --fit_region="region2" &> logs/reweight_cats_region2_log
+
+python main_background_analysis.py --full_analysis --bkg_function 0 \
+                                   --fit_jpsi_prompt --floating_resonant \
+                                   --fit_region="region1" \
+                                   --weight_multiplier 0.01 \
+                                   --tag="div100wgt" &> logs/reweight_cats_region1_div100wgt_log
+
+python main_background_analysis.py --full_analysis --bkg_function 0 \
+                                   --fit_jpsi_prompt --floating_resonant \
+                                   --fit_region="region2" \
+                                   --weight_multiplier 0.01 \
+                                   --tag="div100wgt" &> logs/reweight_cats_region2_div100wgt_log
